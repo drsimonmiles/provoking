36f266801eeceb3cb7f38c8a77050f6cc4ebd9b4
Simon Miles <simon.miles@kcl.ac.uk>
2013-01-15 15:03:46 +0000

    Removed old RDF code and JavaDoc (moved to gh-pages)

--- src/uk/ac/kcl/inf/provoking/model/rdf/BiList.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.kcl.inf.provoking.model.rdf;
-
-import java.util.LinkedList;
-
-class BiList<Type1, Type2> extends LinkedList<Tuple<Type1, Type2>> {
-    void add (Type1 first, Type2 second) {
-        add (new Tuple<> (first, second));
-    }
-}
