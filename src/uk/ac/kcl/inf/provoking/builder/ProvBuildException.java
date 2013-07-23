c1535fdf30b29cf8d0c803078b0194ccedb0341b
Simon Miles <simon.miles@kcl.ac.uk>
2013-07-23 16:39:09 +0100

    Added some JavaDoc

--- src/uk/ac/kcl/inf/provoking/builder/ProvBuildException.java
+++ src/uk/ac/kcl/inf/provoking/builder/ProvBuildException.java
@@ -1,7 +1,36 @@
 package uk.ac.kcl.inf.provoking.builder;
 
+/**
+ * Indicates a failure to execute a build command in using ProvBuilder.
+ * 
+ * @author Simon Miles
+ */
 public class ProvBuildException extends RuntimeException {
+    /**
+     * Create an exception with a given explanation.
+     * 
+     * @param message Explanation of the exception.
+     */
     public ProvBuildException (String message) {
         super (message);
+    }
+    
+    /**
+     * Create an exception with a given cause.
+     * 
+     * @param cause Cause (prior exception) leading to this exception.
+     */
+    public ProvBuildException (Throwable cause) {
+        super (cause);
+    }
+
+    /**
+     * Create an exception with a given cause and explanation.
+     * 
+     * @param cause Cause (prior exception) leading to this exception.
+     * @param message Explanation of the exception.
+     */
+    public ProvBuildException (String message, Throwable cause) {
+        super (message, cause);
     }
 }
